a4b744108bfa49fffa4e4165326837ce9e608886
Ayuka Saijo <ayuka.saijo.0629@gmail.com>
2023-06-28 01:56:01 +0900

    0628

--- js/script.js
+++ js/script.js
@@ -1,3 +1,53 @@
+const loadingAreaGrey=document.querySelector('#loading');
+const loadingAreaGreen=document.querySelector('#loading-screen');
+const loadingText=document.querySelector('#loading p');
+
+window.addEventListener('load',()=>{
+    //ローディング中(グレースクリーン)
+    loadingAreaGrey.animate(
+        {
+            opacity:[1,0],
+            visibility:'hidden',
+        },
+        {
+            duration:2000,
+            delay:1200,
+            easing:'ease',
+            fill:'forwards',
+        }
+    );
+
+    //ローディング中（薄緑スクリーン）
+    loadingAreaGreen.animate(
+        {
+            translate:['0 100vh','0 0','0 -100vh']
+        },
+        {
+            duration:2000,
+            delay:800,
+            easing:'ease',
+            fill:'forwards',
+        }
+    );
+//     ローディング中テキスト
+    loadingText.animate(
+    [
+        {
+            opacity:1,
+            offset:.8 //80%
+        },
+        {
+            opacity:0,
+            offset:1 //100%
+        },
+    ],
+    {
+        duration:1200,
+        easing:'ease',
+        fill:'forwards',
+    }
+    );
+});
 //MAIN-PAGE
 const heading=document.querySelector('#heading');
 
@@ -58,53 +108,3 @@ const odoriObserver=new IntersectionObserver(odori,abu);
 //#odoriを監視するように指示
 odoriObserver.observe(document.querySelector('#odori'));
 
-const loadingAreaGrey=document.querySelector('#loading');
-const loadingAreaGreen=document.querySelector('#loading-screen');
-const loadingText=document.querySelector('#loading p');
-
-window.addEventListener('load',()=>{
-    ローディング中(グレースクリーン)
-    loadingAreaGrey.animate(
-        {
-            opacity:[1,0],
-            visibility:'hidden',
-        },
-        {
-            duration:2000,
-            delay:1200,
-            easing:'ease',
-            fill:'forwards',
-        }
-    );
-
-    //ローディング中（薄緑スクリーン）
-    loadingAreaGreen.animate(
-        {
-            translate:['0 100vh','0 0','0 -100vh']
-        },
-        {
-            duration:2000,
-            delay:800,
-            easing:'ease',
-            fill:'forwards',
-        }
-    );
-    ローディング中テキスト
-    loadingText.animate(
-    [
-        {
-            opacity:1,
-            offset:.8 //80%
-        },
-        {
-            opacity:0,
-            offset:1 //100%
-        },
-    ],
-    {
-        duration:1200,
-        easing:'ease',
-        fill:'forwards',
-    }
-    );
-});
